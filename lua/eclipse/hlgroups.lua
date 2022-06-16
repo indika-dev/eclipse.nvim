ddc9c747e2977c1bd224d83864a903cf0eebe5c1
Stefan Maaßen <56093026+indika-dev@users.noreply.github.com>
2022-06-17 00:16:55 +0200

    searching missing fg

--- lua/eclipse/hlgroups.lua
+++ lua/eclipse/hlgroups.lua
@@ -149,155 +149,155 @@ function M.setup(colors, config)
 		-- LspReferenceRead = { bg = colors.fg_dark },
 		-- LspReferenceWrite = { bg = "Orange" },
 
-		DiagnosticError = { fg = colors.diag.error.fg, bg = colors.diag.error.bg },
-		DiagnosticWarn = { fg = colors.diag.warning.fg, bg = colors.diag.warning.bg },
-		DiagnosticInfo = { fg = colors.diag.info.fg, bg = colors.diag.info.bg },
-		DiagnosticHint = { fg = colors.diag.hint.fg, bg = colors.diag.hint.bg },
+		-- DiagnosticError = { fg = colors.diag.error.fg, bg = colors.diag.error.bg },
+		-- DiagnosticWarn = { fg = colors.diag.warning.fg, bg = colors.diag.warning.bg },
+		-- DiagnosticInfo = { fg = colors.diag.info.fg, bg = colors.diag.info.bg },
+		-- DiagnosticHint = { fg = colors.diag.hint.fg, bg = colors.diag.hint.bg },
 
-		DiagnosticSignError = { link = "DiagnosticError" },
-		DiagnosticSignWarn = { link = "DiagnosticWarn" },
-		DiagnosticSignInfo = { link = "DiagnosticInfo" },
-		DiagnosticSignHint = { link = "DiagnosticHint" },
+		-- DiagnosticSignError = { link = "DiagnosticError" },
+		-- DiagnosticSignWarn = { link = "DiagnosticWarn" },
+		-- DiagnosticSignInfo = { link = "DiagnosticInfo" },
+		-- DiagnosticSignHint = { link = "DiagnosticHint" },
 
-		DiagnosticVirtualTextError = { link = "DiagnosticError" },
-		DiagnosticVirtualTextWarn = { link = "DiagnosticWarn" },
-		DiagnosticVirtualTextInfo = { link = "DiagnosticInfo" },
-		DiagnosticVirtualTextHint = { link = "DiagnosticHint" },
+		-- DiagnosticVirtualTextError = { link = "DiagnosticError" },
+		-- DiagnosticVirtualTextWarn = { link = "DiagnosticWarn" },
+		-- DiagnosticVirtualTextInfo = { link = "DiagnosticInfo" },
+		-- DiagnosticVirtualTextHint = { link = "DiagnosticHint" },
 
-		DiagnosticUnderlineError = { undercurl = true, sp = colors.diag.error.bg },
-		DiagnosticUnderlineWarn = { undercurl = true, sp = colors.diag.warning.bg },
-		DiagnosticUnderlineInfo = { undercurl = true, sp = colors.diag.info.bg },
-		DiagnosticUnderlineHint = { undercurl = true, sp = colors.diag.hint.bg },
+		-- DiagnosticUnderlineError = { undercurl = true, sp = colors.diag.error.bg },
+		-- DiagnosticUnderlineWarn = { undercurl = true, sp = colors.diag.warning.bg },
+		-- DiagnosticUnderlineInfo = { undercurl = true, sp = colors.diag.info.bg },
+		-- DiagnosticUnderlineHint = { undercurl = true, sp = colors.diag.hint.bg },
 
-		LspSignatureActiveParameter = { fg = colors.diag.warning.fg, bg = colors.diag.warning.bg },
-		LspCodeLens = { fg = colors.comment.fg },
+		-- LspSignatureActiveParameter = { fg = colors.diag.warning.fg, bg = colors.diag.warning.bg },
+		-- LspCodeLens = { fg = colors.comment.fg },
 
-		-- ALEErrorSign = {},
-		-- ALEWarningSign = {},
+		-- -- ALEErrorSign = {},
+		-- -- ALEWarningSign = {},
 
-		-- TSAnnotation = {},
-		TSAttribute = { link = "Constant" },
-		-- TSBoolean = {},
-		-- TSCharacter = {},
-		-- TSComment = {},
-		-- TSNote = { fg = c.fg_dark, bg = c.diag.hint, nocombine = true, bold = true}, -- links to SpecialComment -> Special
-		TSWarning = { link = "Todo" }, --default
-		TSDanger = { link = "WarningMsg" }, --default
-		TSConstructor = { fg = colors.keyword.fg }, -- Function/Special/Statement/Keyword
-		-- TSConditional = {},
-		-- TSConstant = {},
-		-- TSConstBuiltin = {},
-		-- TSConstMacro = {},
-		-- TSError = { fg = colors.diag.error },
-		-- TSException = { link = 'Exception' }, -- default, -> statement
-		TSException = vim.tbl_extend(
-			"force",
-			{ fg = config.specialException and colors.special.fg or colors.statement.fg },
-			config.statementStyle
-		),
-		TSField = { link = "Identifier" }, -- default
-		-- TSField = { link = 'Variable'},
-		-- TSFloat = {},
-		-- TSFunction = {},
-		-- TSFuncBuiltin = {link = "Function" },
-		-- TSFuncMacro = {},
-		-- TSInclude = {},
-		TSKeyword = { link = "Keyword" },
-		-- TSKeywordFunction = { link = "Keyword" }, -- default
-		-- TSKeywordFunction = { link = "Function" },
-		TSKeywordReturn = vim.tbl_extend(
-			"force",
-			{ fg = config.specialReturn and colors.special.fg or colors.keyword },
-			config.keywordStyle
-		),
-		TSLabel = { link = "Label" },
-		TSMethod = { link = "Function" },
-		-- TSNamespace = {},
-		-- TSNone = {},
-		-- TSNumber = {},
-		TSOperator = { link = "Operator" },
-		TSKeywordOperator = { fg = colors.operator.fg, bold = true },
-		TSParameter = { link = "Identifier" }, -- default
-		-- TSParameterReference = {},
-		TSProperty = { link = "Identifier" }, -- default
-		-- TSPunctDelimiter = { fg = c.op },
-		TSPunctDelimiter = { fg = colors.fg },
-		TSPunctBracket = { link = "TSPunctDelimiter" },
-		TSPunctSpecial = { link = "TSPunctDelimiter" },
-		-- TSRepeat = {},
-		-- TSString = {},
-		TSStringRegex = { fg = colors.regex.fg },
-		TSStringEscape = { fg = colors.regex.fg, bold = true },
-		-- TSSymbol = {},
-		-- TSType = {},
-		-- TSTypeBuiltin = {},
-		TSVariable = { fg = colors.fg },
-		TSVariableBuiltin = vim.tbl_extend("force", { fg = colors.special.fg }, config.variablebuiltinStyle),
+		-- -- TSAnnotation = {},
+		-- TSAttribute = { link = "Constant" },
+		-- -- TSBoolean = {},
+		-- -- TSCharacter = {},
+		-- -- TSComment = {},
+		-- -- TSNote = { fg = c.fg_dark, bg = c.diag.hint, nocombine = true, bold = true}, -- links to SpecialComment -> Special
+		-- TSWarning = { link = "Todo" }, --default
+		-- TSDanger = { link = "WarningMsg" }, --default
+		-- TSConstructor = { fg = colors.keyword.fg }, -- Function/Special/Statement/Keyword
+		-- -- TSConditional = {},
+		-- -- TSConstant = {},
+		-- -- TSConstBuiltin = {},
+		-- -- TSConstMacro = {},
+		-- -- TSError = { fg = colors.diag.error },
+		-- -- TSException = { link = 'Exception' }, -- default, -> statement
+		-- TSException = vim.tbl_extend(
+		-- 	"force",
+		-- 	{ fg = config.specialException and colors.special.fg or colors.statement.fg },
+		-- 	config.statementStyle
+		-- ),
+		-- TSField = { link = "Identifier" }, -- default
+		-- -- TSField = { link = 'Variable'},
+		-- -- TSFloat = {},
+		-- -- TSFunction = {},
+		-- -- TSFuncBuiltin = {link = "Function" },
+		-- -- TSFuncMacro = {},
+		-- -- TSInclude = {},
+		-- TSKeyword = { link = "Keyword" },
+		-- -- TSKeywordFunction = { link = "Keyword" }, -- default
+		-- -- TSKeywordFunction = { link = "Function" },
+		-- TSKeywordReturn = vim.tbl_extend(
+		-- 	"force",
+		-- 	{ fg = config.specialReturn and colors.special.fg or colors.keyword },
+		-- 	config.keywordStyle
+		-- ),
+		-- TSLabel = { link = "Label" },
+		-- TSMethod = { link = "Function" },
+		-- -- TSNamespace = {},
+		-- -- TSNone = {},
+		-- -- TSNumber = {},
+		-- TSOperator = { link = "Operator" },
+		-- TSKeywordOperator = { fg = colors.operator.fg, bold = true },
+		-- TSParameter = { link = "Identifier" }, -- default
+		-- -- TSParameterReference = {},
+		-- TSProperty = { link = "Identifier" }, -- default
+		-- -- TSPunctDelimiter = { fg = c.op },
+		-- TSPunctDelimiter = { fg = colors.fg },
+		-- TSPunctBracket = { link = "TSPunctDelimiter" },
+		-- TSPunctSpecial = { link = "TSPunctDelimiter" },
+		-- -- TSRepeat = {},
+		-- -- TSString = {},
+		-- TSStringRegex = { fg = colors.regex.fg },
+		-- TSStringEscape = { fg = colors.regex.fg, bold = true },
+		-- -- TSSymbol = {},
+		-- -- TSType = {},
+		-- -- TSTypeBuiltin = {},
+		-- TSVariable = { fg = colors.fg },
+		-- TSVariableBuiltin = vim.tbl_extend("force", { fg = colors.special.fg }, config.variablebuiltinStyle),
 
-		-- TSTag = {},
-		-- TSTagDelimiter = {},
-		-- TSText = {},
-		-- TSTextReference = { fg = c.sp2 },
-		-- TSEmphasis = {},
-		-- TSUnderline = {},
-		-- TSStrike = {},
-		-- TSTitle = {},
-		-- TSLiteral = {},
-		-- TSURI = {},
+		-- -- TSTag = {},
+		-- -- TSTagDelimiter = {},
+		-- -- TSText = {},
+		-- -- TSTextReference = { fg = c.sp2 },
+		-- -- TSEmphasis = {},
+		-- -- TSUnderline = {},
+		-- -- TSStrike = {},
+		-- -- TSTitle = {},
+		-- -- TSLiteral = {},
+		-- -- TSURI = {},
 
-		-- Lua
-		-- luaTSProperty = {},
+		-- -- Lua
+		-- -- luaTSProperty = {},
 
-		-- LspTrouble
-		-- LspTroubleText = {},
-		-- LspTroubleCount = {},
-		-- LspTroubleNormal = {},
+		-- -- LspTrouble
+		-- -- LspTroubleText = {},
+		-- -- LspTroubleCount = {},
+		-- -- LspTroubleNormal = {},
 
-		-- Illuminate
-		-- illuminatedWord = {},
-		-- illuminatedCurWord = {},
+		-- -- Illuminate
+		-- -- illuminatedWord = {},
+		-- -- illuminatedCurWord = {},
 
-		-- Git
-		diffAdded = { fg = colors.git.added.fg, bg = colors.git.added.bg },
-		diffRemoved = { fg = colors.git.removed.fg, bg = colors.git.removed.bg },
-		diffDeleted = { fg = colors.git.removed.fg, bg = colors.git.removed.bg },
-		diffChanged = { fg = colors.git.changed.fg, bg = colors.git.changed.bg },
-		diffOldFile = { fg = colors.git.removed.fg, bg = colors.git.removed.bg },
-		diffNewFile = { fg = colors.git.added.fg, bg = colors.git.added.bg },
-		-- diffFile = { fg = c.steelGray },
-		-- diffLine = { fg = c.steelGray },
-		-- diffIndexLine = { link = 'Identifier' },
+		-- -- Git
+		-- diffAdded = { fg = colors.git.added.fg, bg = colors.git.added.bg },
+		-- diffRemoved = { fg = colors.git.removed.fg, bg = colors.git.removed.bg },
+		-- diffDeleted = { fg = colors.git.removed.fg, bg = colors.git.removed.bg },
+		-- diffChanged = { fg = colors.git.changed.fg, bg = colors.git.changed.bg },
+		-- diffOldFile = { fg = colors.git.removed.fg, bg = colors.git.removed.bg },
+		-- diffNewFile = { fg = colors.git.added.fg, bg = colors.git.added.bg },
+		-- -- diffFile = { fg = c.steelGray },
+		-- -- diffLine = { fg = c.steelGray },
+		-- -- diffIndexLine = { link = 'Identifier' },
 
-		-- Neogit
-		-- NeogitBranch = {},
-		-- NeogitRemote = {},
-		-- NeogitHunkHeader = {},
-		-- NeogitHunkHeaderHighlight = {},
-		-- NeogitDiffContextHighlight = {},
-		-- NeogitDiffDeleteHighlight = {},
-		-- NeogitDiffAddHighlight = {},
+		-- -- Neogit
+		-- -- NeogitBranch = {},
+		-- -- NeogitRemote = {},
+		-- -- NeogitHunkHeader = {},
+		-- -- NeogitHunkHeaderHighlight = {},
+		-- -- NeogitDiffContextHighlight = {},
+		-- -- NeogitDiffDeleteHighlight = {},
+		-- -- NeogitDiffAddHighlight = {},
 
-		-- GitGutter
-		-- GitGutterAdd = {},
-		-- GitGutterChange = {},
-		-- GitGutterDelete = {},
+		-- -- GitGutter
+		-- -- GitGutterAdd = {},
+		-- -- GitGutterChange = {},
+		-- -- GitGutterDelete = {},
 
-		-- GitSigns
-		GitSignsAdd = { link = "diffAdded" },
-		GitSignsChange = { link = "diffChanged" },
-		GitSignsDelete = { link = "diffDeleted" },
-		GitSignsDeleteLn = { bg = colors.diff.delete.bg },
+		-- -- GitSigns
+		-- GitSignsAdd = { link = "diffAdded" },
+		-- GitSignsChange = { link = "diffChanged" },
+		-- GitSignsDelete = { link = "diffDeleted" },
+		-- GitSignsDeleteLn = { bg = colors.diff.delete.bg },
 
-		-- Telescope                      = {},
-		TelescopeBorder = { fg = colors.vertSplit.fg, bg = colors.vertSplit.bg },
-		TelescopeResultsClass = { link = "TSType" },
-		TelescopeResultsStruct = { link = "TSType" },
-		TelescopeResultsVariable = { link = "TSVariable" },
+		-- -- Telescope                      = {},
+		-- TelescopeBorder = { fg = colors.vertSplit.fg, bg = colors.vertSplit.bg },
+		-- TelescopeResultsClass = { link = "TSType" },
+		-- TelescopeResultsStruct = { link = "TSType" },
+		-- TelescopeResultsVariable = { link = "TSVariable" },
 
-		-- NvimTree                       = {},
-		NvimTreeNormal = { link = "Normal" },
-		NvimTreeNormalNC = { link = "NormalNC" },
-		NvimTreeRootFolder = { fg = colors.title.fg, bold = true },
+		-- -- NvimTree                       = {},
+		-- NvimTreeNormal = { link = "Normal" },
+		-- NvimTreeNormalNC = { link = "NormalNC" },
+		-- NvimTreeRootFolder = { fg = colors.title.fg, bold = true },
 		NvimTreeGitDirty = { fg = colors.git.changed.fg, bg = colors.git.changed.bg },
 		NvimTreeGitNew = { fg = colors.git.added.fg, bg = colors.git.added.bg },
 		NvimTreeGitDeleted = { bg = colors.git.removed.bg, fg = "Red" },
